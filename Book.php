d4d8f6b64ee8058ce5d24de004d2d240c43799f8
RahatSiddique2001 <rahatsiddique2001@gmail.com>
2023-10-17 00:12:38 +0600

    Book and Customer file almost done.

--- Book.php
+++ Book.php
@@ -7,6 +7,63 @@ class Book
     private $author;
     private $available;
 
-    
+    public function __construct($isbn, $title, $author, $available)
+    {
+        $this->isbn = $isbn;
+        $this->title = $title;
+        $this->author = $author;
+        $this->available = $available;
+    }
+
+    public function addCopy($num)
+    {
+        if ($num > 0) {
+            $this->available += $num;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getCopy()
+    {
+        if ($this->available > 0) {
+            $this->available--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function __toString()
+    {
+        return "ISBN: {$this->isbn}, Title: {$this->title}, Author: {$this->author}, Available Copies: {$this->available}";
+    }
+
+    public function __call($name, $arguments)
+    {
+        $action = substr($name, 0, 3);
+        $property = lcfirst(substr($name, 3));
+        
+        if ($action === "get") {
+            return $this->$property;
+        } elseif ($action === "set") {
+            $this->$property = $arguments[0];
+        }
+    }
+
+    public function __get($name)
+    {
+        if (property_exists($this, $name)) {
+            return $this->$name;
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        if (property_exists($this, $name)) {
+            $this->$name = $value;
+        }
+    }
 }
 ?>
--- Customer.php
+++ Customer.php
@@ -7,6 +7,43 @@ class Customer
     private $lastName;
     private $email;
 
-    
+    public function __construct($id, $firstName, $lastName, $email)
+    {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+    }
+
+    public function __toString()
+    {
+        return "Customer ID: {$this->id}, Name: {$this->firstName} {$this->lastName}, Email: {$this->email}";
+    }
+
+    public function __call($name, $arguments)
+    {
+        $action = substr($name, 0, 3);
+        $property = lcfirst(substr($name, 3));
+        
+        if ($action === "get") {
+            return $this->$property;
+        } elseif ($action === "set") {
+            $this->$property = $arguments[0];
+        }
+    }
+
+    public function __get($name)
+    {
+        if (property_exists($this, $name)) {
+            return $this->$name;
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        if (property_exists($this, $name)) {
+            $this->$name = $value;
+        }
+    }
 }
 ?>
